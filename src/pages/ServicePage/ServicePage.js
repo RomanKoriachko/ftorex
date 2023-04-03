9c525087a70d0e31efa67b09b9d974dd4f324ae5
Roman <mazaxaka.tyt@gmail.com>
2023-04-03 17:20:45 +0300

    add articles in ServicePage

--- src/pages/ServicePage/ServicePage.js
+++ src/pages/ServicePage/ServicePage.js
@@ -1,7 +1,151 @@
 import React from 'react'
+import ScrollDownBtn from '../../components/ScrollDownBtn/ScrollDownBtn'
+import './ServicePage.scss'
 
 const ServicePage = () => {
-    return <div>ServicePage</div>
+    return (
+        <div className="service-page">
+            <section className="preview-section">
+                <div className="container">
+                    <div className="preview-bg">
+                        <p className="service-header">Service</p>
+                        <div className="scroll-btn">
+                            <ScrollDownBtn />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="supplies-section">
+                <div className="container">
+                    <p className="supplies-header">
+                        Ftorex is a global coal industry company designed to
+                        establish and develop a Coal Business, Engineering &
+                        General Purpose
+                    </p>
+                    <div className="row paragraph-row">
+                        <img src="images/supplies-img-1.png" alt="" />
+                        <div>
+                            <p className="item-header">Coal supplies</p>
+                            <p className="item-text">
+                                At FTOREX, we prioritize quality and
+                                sustainability, and we work with manufacturers
+                                who share our commitment to these values. We
+                                believe that responsible sourcing and ethical
+                                business practices are essential for building
+                                long-term relationships with our customers and
+                                ensuring the continued success of our business.
+                            </p>
+                            <p className="item-text">
+                                Overall, FTOREX Commercial Broker LLC is a
+                                reliable and reputable supplier of coking coal
+                                to customers worldwide.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="row paragraph-row">
+                        <div>
+                            <p className="item-header">Fertilizer supplies</p>
+                            <p className="item-text">
+                                At FTOREX, we prioritize quality and
+                                sustainability, and we work with manufacturers
+                                who share our commitment to these values. We
+                                believe that responsible sourcing and ethical
+                                business practices are essential for building
+                                long-term relationships with our customers and
+                                ensuring the continued success of our business.
+                            </p>
+                            <p className="item-text">
+                                Overall, FTOREX Commercial Broker LLC is a
+                                reliable and reputable supplier of coking coal
+                                to customers worldwide.
+                            </p>
+                        </div>
+                        <img src="images/supplies-img-2.png" alt="" />
+                    </div>
+                    <div className="row paragraph-row">
+                        <img src="images/supplies-img-3.png" alt="" />
+                        <div>
+                            <p className="item-header">Logistic Solutions</p>
+                            <p className="item-text">
+                                At FTOREX, we prioritize quality and
+                                sustainability, and we work with manufacturers
+                                who share our commitment to these values. We
+                                believe that responsible sourcing and ethical
+                                business practices are essential for building
+                                long-term relationships with our customers and
+                                ensuring the continued success of our business.
+                            </p>
+                            <p className="item-text">
+                                Overall, FTOREX Commercial Broker LLC is a
+                                reliable and reputable supplier of coking coal
+                                to customers worldwide.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="row paragraph-row">
+                        <div>
+                            <p className="item-header">Customs Clearance</p>
+                            <p className="item-text">
+                                At FTOREX, we prioritize quality and
+                                sustainability, and we work with manufacturers
+                                who share our commitment to these values. We
+                                believe that responsible sourcing and ethical
+                                business practices are essential for building
+                                long-term relationships with our customers and
+                                ensuring the continued success of our business.
+                            </p>
+                            <p className="item-text">
+                                Overall, FTOREX Commercial Broker LLC is a
+                                reliable and reputable supplier of coking coal
+                                to customers worldwide.
+                            </p>
+                        </div>
+                        <img src="images/supplies-img-4.png" alt="" />
+                    </div>
+                    <div className="row paragraph-row">
+                        <img src="images/supplies-img-5.png" alt="" />
+                        <div>
+                            <p className="item-header">Ocean freight</p>
+                            <p className="item-text">
+                                At FTOREX, we prioritize quality and
+                                sustainability, and we work with manufacturers
+                                who share our commitment to these values. We
+                                believe that responsible sourcing and ethical
+                                business practices are essential for building
+                                long-term relationships with our customers and
+                                ensuring the continued success of our business.
+                            </p>
+                            <p className="item-text">
+                                Overall, FTOREX Commercial Broker LLC is a
+                                reliable and reputable supplier of coking coal
+                                to customers worldwide.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="row paragraph-row">
+                        <div>
+                            <p className="item-header">Consulting</p>
+                            <p className="item-text">
+                                At FTOREX, we prioritize quality and
+                                sustainability, and we work with manufacturers
+                                who share our commitment to these values. We
+                                believe that responsible sourcing and ethical
+                                business practices are essential for building
+                                long-term relationships with our customers and
+                                ensuring the continued success of our business.
+                            </p>
+                            <p className="item-text">
+                                Overall, FTOREX Commercial Broker LLC is a
+                                reliable and reputable supplier of coking coal
+                                to customers worldwide.
+                            </p>
+                        </div>
+                        <img src="images/supplies-img-6.png" alt="" />
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
 }
 
 export default ServicePage
