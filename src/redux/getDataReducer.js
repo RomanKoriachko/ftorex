96256314b3bdf79d6a58d71db0c00a2e392db987
Roman <mazaxaka.tyt@gmail.com>
2023-04-07 16:42:49 +0300

    add api key

--- src/redux/getDataReducer.js
+++ src/redux/getDataReducer.js
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 
 export const fetchData = createAsyncThunk("fetchData", async function () {
     const response = await fetch(
-        "https://commodities-api.com/api/latest?access_key=n6yxs7lp3z33zr29k1r77gqsikzx0ohw8u467x2f6awogyboicno012fjum6&base=USD&symbols=RICE%2CWHEAT%2CSUGAR%2CCORN%2CWTIOIL%2CBRENTOIL%2CSOYBEAN%2CCOFFEE%2CXAU%2CXAG%2CXPD%2CXPT%2CXRH%2CRUBBER%2CETHANOL%2CCPO%2CNG%2CLUMBER%2CCOTTON%2CCOAL%2CCOCOA"
+        "https://commodities-api.com/api/latest?access_key=2io44hde9b6qqs2zj046i24v51w1l8s9pt1kfh41baku3t2wh7hbl7o83vx6&base=USD&symbols=RICE%2CWHEAT%2CSUGAR%2CCORN%2CWTIOIL%2CBRENTOIL%2CSOYBEAN%2CCOFFEE%2CXAU%2CXAG%2CXPD%2CXPT%2CXRH%2CRUBBER%2CETHANOL%2CCPO%2CNG%2CLUMBER%2CCOTTON%2CCOAL%2CCOCOA"
     )
     const data = await response.json()
     return data
