0a5a390b6d6f06ce760671bf4a21c8baf94ce2f8
Roman <mazaxaka.tyt@gmail.com>
2023-04-15 00:17:38 +0300

    add tablet map

--- src/components/MapSection/MapSection.js
+++ src/components/MapSection/MapSection.js
@@ -7,8 +7,6 @@ const MapSection = ({ ref5 }) => {
     const showMap = () => {
         if (showMapState === "not-active" && window.innerWidth > 992) {
             setShowMapState("active")
-        } else if (showMapState === "not-active" && window.innerWidth <= 992) {
-            setShowMapState("tablet-active")
         } else {
             setShowMapState("not-active")
         }
@@ -16,6 +14,20 @@ const MapSection = ({ ref5 }) => {
 
     const closeMap = () => {
         setShowMapState("not-active")
+    }
+
+    const [tabletMapState, setTabletMapState] = useState("hide-map")
+
+    const showTabletMap = () => {
+        if (tabletMapState === "hide-map") {
+            setTabletMapState("show-map")
+        } else {
+            setTabletMapState("hide-map")
+        }
+    }
+
+    const closeTabletMap = () => {
+        setTabletMapState("hide-map")
     }
 
     return (
@@ -30,7 +42,16 @@ const MapSection = ({ ref5 }) => {
                     <div className="btn-arrow"></div>
                 </div>
             </button>
-            <div className={`map-wrapper ${showMapState}`}>
+            <button
+                className={`tablet-close-map-btn ${tabletMapState}`}
+                onClick={() => closeTabletMap()}
+            >
+                <div className="row btn-content-row">
+                    <p>show less</p>
+                    <div className="btn-arrow"></div>
+                </div>
+            </button>
+            <div className={`desktop-map-wrapper map-wrapper ${showMapState}`}>
                 <div className="map-content">
                     <div className="map-text">
                         <div className="tablet-row">
@@ -47,6 +68,42 @@ const MapSection = ({ ref5 }) => {
                             resources. Start exploring today!
                         </p>
                         <button className="map-btn" onClick={() => showMap()}>
+                            <div className="row btn-content-row">
+                                <p>click and explore</p>
+                                <div className="btn-arrow"></div>
+                            </div>
+                        </button>
+                    </div>
+                </div>
+                <div className="map-overlap">
+                    <iframe
+                        className="map"
+                        src="https://greeninfo-network.github.io/mine-tracker/"
+                        title="https://greeninfo-network.github.io/mine-tracker/"
+                        frameborder="0"
+                    ></iframe>
+                </div>
+            </div>
+            <div className={`tablet-map-wrapper map-wrapper ${tabletMapState}`}>
+                <div className="map-content">
+                    <div className="map-text">
+                        <div className="tablet-row">
+                            <p className="big-header">Deposit</p>
+                            <p className="big-header">map</p>
+                        </div>
+                        <p className="subtitle">
+                            Welcome to Deposit Map section! Here, you can
+                            explore all the deposits of our planet. Every
+                            deposit is marked, making it easier for you to find
+                            the resources you need. Whether you're a researcher,
+                            a miner, or an investor, this map is a valuable tool
+                            for anyone interested in understanding the Earth's
+                            resources. Start exploring today!
+                        </p>
+                        <button
+                            className="map-btn"
+                            onClick={() => showTabletMap()}
+                        >
                             <div className="row btn-content-row">
                                 <p>click and explore</p>
                                 <div className="btn-arrow"></div>
